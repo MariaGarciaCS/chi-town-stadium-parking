8fe85f0781b9472f69ce76175723501eea989189
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-09-16 21:03:24 -0500

    added date, time, img and dotenv

--- src/components/API/FetchData.jsx
+++ src/components/API/FetchData.jsx
@@ -1,31 +1,45 @@
 import { useState, useEffect } from "react"
 
-
 export default function FetchData() {
-  const [events, setevents] = useState();
+  const [events, setEvents] = useState();
   
-  const tm_api = ''
+  const tm_api = process.env.REACT_APP_API_KEY;
   const tm_url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${tm_api}&venueId=KovZpZAF6tIA&locale=*`
 
   useEffect(() =>{
-    fetch(tm_url)
-    .then((response) =>  response.json())
-    .then((json) => {
-      console.log(json._embedded.events)
-      setevents(json._embedded.events)
-    })
+    getEvents()
   }, []);
 
-  console.log(typeof(events.name))
+  const getEvents = async () =>{
+    const response = await fetch(tm_url).then((res) => res.json());
 
-  console.log(events[0].name)
+    setEvents(await response._embedded.events)
+  }
+
+  // fetch(tm_url)
+  //   .then((response) =>  response.json())
+  //   .then((json) => {
+  //     console.log(json._embedded.events)
+  //     setevents(json._embedded.events)
+  //   })
+  //   .catch((err) => console.log(err))
 
   return (
     <>
-      <h2>Upcoming Events</h2>
-      {events.map((event) => {
-        return <p>{event.name}</p>
-      })}
+      <div>
+        <ul>
+          {
+            events.map((event, index) => {
+              return <div key={index} className="event">
+                 <h2>{index + 1}. {event.name}</h2>
+                 <p>{event.dates.start.localDate}</p>
+                 <p>{event.dates.start.localTime}</p>
+                 <img src={event.images[1].url} alt="" />
+                </div>
+            })
+          }
+        </ul>
+      </div>
     </>
   )
 }
